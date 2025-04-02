3a9407e260de28c554748d2e471e986c90e7fcdb
AK5875 <std164116@ac.eap.gr>
2025-04-02 20:17:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,6 @@ import requests  "σύνδεση με το API"
 import json "για μετατροπή των Data που θα παίρνουμε από τον server Trivial"
 import time "για το χρονόμετρο"
 
-import json
-import requests
-
 # θα πρέπει να προστεθεί το μήνυμα καλωσορίσματος
 
 def display_erotiseis():                                                                            #συνάρτηση δημιουργίας μενού επιλογής ερωτήσεων
